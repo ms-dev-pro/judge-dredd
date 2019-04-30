dc79e940f3ab71567f4651d268ecb411dfa0e5ba
ms-dev-pro <sebastien.marchal@protonmail.com>
2019-04-30 11:30:00 +0200

    try other config

--- api/core.py
+++ api/core.py
@@ -10,7 +10,7 @@ app.debug = True
 
 app.config['LDAP_HOST'] = 'controler.epsi.intra'
 app.config['LDAP_BASE_DN'] = 'cn=Users,dc=epsi,dc=intra'
-app.config['LDAP_USERNAME'] = 'Administrator'
+app.config['LDAP_USERNAME'] = 'cn=Administrator,cn=Users,dc=epsi,dc=intra'
 app.config['LDAP_PASSWORD'] = 'P@ssw0rd'
 
 
