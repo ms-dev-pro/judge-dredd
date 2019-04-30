326271f41d25a74dcdc28a1af361a4bfb83091be
ms-dev-pro <sebastien.marchal@protonmail.com>
2019-04-30 03:08:10 +0200

    Added first ldap authentication try

--- api/core.py
+++ api/core.py
@@ -1,14 +1,37 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
+from flask_simpleldap import LDAP
+
 import uuid
 import json
 
 app = Flask(__name__)
 
+app.config['LDAP_BASE_DN'] = 'OU=I4,dc=epsi,dc=intra'
+app.config['LDAP_USERNAME'] = 'CN=Administrator,OU=Users,DC=epsi,DC=intra'
+app.config['LDAP_PASSWORD'] = 'P@ssw0rd'
+
+ldap = LDAP(app)
 
 @app.route('/')
 def mainRoute():
     return handleResponse(200, 'text/plain', 'Welcome to Judge-Dredd API !')
 
+@app.route('/login', methods = ['POST'])
+def loginRoute():
+    if request.method == 'POST':
+        user = request.get_json()
+        #print(user['username'])
+        #print(user['password'])
+        test = ldap.bind_user(user['username'], user['password'])
+        if test is None or user['password'] == '':
+            return 'Invalid credentials'
+        else:
+            return redirect('/logged')
+
+@app.route('/logged')
+@ldap.login_required
+def loggedRoute():
+    return 'Successfully logged in!'
 
 def handleErrors(statuscode, errorCode):
     switcher = {
