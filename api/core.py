abaa334a689b1cf806bbcbf8b43a68a545ab4e42
ms-dev-pro <sebastien.marchal@protonmail.com>
2019-05-01 12:07:50 +0200

    added missing import

--- api/core.py
+++ api/core.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, redirect, session, url_for
+from flask import Flask, g, request, Response, redirect, session, url_for
 from flask_simpleldap import LDAP
 
 import uuid
